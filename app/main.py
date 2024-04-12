750d04731b5d2865c63dd8320df6bb9c748fb17f
윤서준 <79549525+yunjs-code@users.noreply.github.com>
2024-04-12 17:26:06 +0900

    12
    12

--- app/main.py
+++ app/main.py
@@ -1,30 +1,22 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+    name: str
+
 items = {}
 
-@app.post("/items/{item_name}")
-def create_item(item_name: str):
-    if item_name in items:
-        return {"Error": "Item already exists"}
-    items[item_name] = item_name
-    return {"item_name": item_name}
+@app.post("/items/")
+def create_item(item: Item):
+    if item.name in items:
+        return {"Error": "Item already exists."}
+    items[item.name] = item
+    return item
 
 @app.get("/items/{item_name}")
 def read_item(item_name: str):
-    return {"item_name": items.get(item_name, "Not found")}
-
-@app.put("/items/{item_name}")
-def update_item(old_name: str, new_name: str):
-    if old_name not in items:
-        return {"Error": "Item not found"}
-    items[new_name] = items.pop(old_name)
-    return {"item_name": new_name}
-
-@app.delete("/items/{item_name}")
-def delete_item(item_name: str):
-    if item_name not in items:
-        return {"Error": "Item not found"}
-    del items[item_name]
-    return {"message": "Item deleted successfully"}
+    if item_name in items:
+        return items[item_name]
+    return {"Error": "Item not found."}
