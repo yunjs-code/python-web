35feb3f0cd790d1e5fd42864a8773695fe66663b
윤서준 <79549525+yunjs-code@users.noreply.github.com>
2024-04-19 15:09:37 +0900

    12
    12

--- app/main.py
+++ app/main.py
@@ -1,14 +1,13 @@
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
-@app.get("/fetch-power-usage")
-async def fetch_power_usage():
+@app.get("/get-power-usage")
+def get_power_usage():
     url = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do"
-    # API 키와 필요한 파라미터 설정
     params = {
-        "access_key": "38M6ESlslJK96g1366SPy0h11fsRr6os9cz2bRFF",  # 여기에 실제 액세스 키를 입력하세요.
+        "api_key": "38M6ESlslJK96g1366SPy0h11fsRr6os9cz2bRFF",
     }
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url, params=params)
-        return response.json()
+    response = requests.get(url, params=params)
+    return response.json()
