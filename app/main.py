6854dc4e7949884a0cc2a6bea1c1fb22df58c392
윤서준 <79549525+yunjs-code@users.noreply.github.com>
2024-04-19 16:01:53 +0900

    12
    12

--- app/main.py
+++ app/main.py
@@ -5,7 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&apiKey=xxx&returnType=json"
+    URL = "	https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&apiKey=38M6ESlslJK96g1366SPy0h11fsRr6os9cz2bRFF&returnType=json"
+    
     contents = requests.get(URL).text
     return {"message": contents}
 
