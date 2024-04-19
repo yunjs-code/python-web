b906f94c22433a0ed1c518960b695e05da95249f
윤서준 <79549525+yunjs-code@users.noreply.github.com>
2024-04-19 14:45:44 +0900

    12
    12

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
+# 파이썬 공식 이미지 사용
 FROM python:3.12
 
+# 작업 디렉토리 설정
 WORKDIR /code
 
+# 의존성 파일 복사 후 설치
 COPY ./requirements.txt /code/requirements.txt
-
 RUN pip3 install --no-cache-dir --upgrade -r /code/requirements.txt
+
+# 애플리케이션 코드 복사
+COPY ./app /code/app
+
+# 서버 실행
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
--- app/main.py
+++ app/main.py
@@ -1,26 +1,15 @@
-from fastapi import FastAPI, Query
-from pydantic import BaseModel
+from fastapi import FastAPI
+import httpx
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-
-items = {}
-
-@app.post("/items/")
-def create_item(item: Item):
-    if item.name in items:
-        return {"Error": "Item already exists."}
-    items[item.name] = item
-    return item
-
-@app.get("/items/{item_name}")
-def read_item(item_name: str):
-    if item_name in items:
-        return items[item_name]
-    return {"Error": "Item not found."}
-
-@app.get("/echo/")
-def echo_name(name: str = Query(None, description="Enter your name to echo")):
-    return {"Echoed Name": name}
+@app.get("/fetch-power-usage")
+async def fetch_power_usage():
+    url = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do"
+    # API 키와 필요한 파라미터 설정
+    params = {
+        "access_key": "38M6ESlslJK96g1366SPy0h11fsRr6os9cz2bRFF",  # 여기에 실제 액세스 키를 입력하세요.
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params=params)
+        return response.json()
