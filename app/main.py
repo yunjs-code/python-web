8f6960cb50db7adfddb6fbc43279977693bbea25
윤서준 <79549525+yunjs-code@users.noreply.github.com>
2024-04-19 15:38:19 +0900

    12
    12

--- app/main.py
+++ app/main.py
@@ -3,18 +3,12 @@ import requests
 
 app = FastAPI()
 
-@app.get("/get-power-usage/{year}/{month}/{metroCd}/{cityCd}")
-def get_power_usage(year: int, month: int, metroCd: str, cityCd: str):
-    url = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do"
-    params = {
-        "apiKey": "38M6ESlslJK96g1366SPy0h11fsRr6os9cz2bRFF",  # 환경 변수에서 가져오는 것을 추천
-        "year": year,
-        "month": month,
-        "metroCd": metroCd,
-        "cityCd": cityCd,
-        "returnType": "json"
-    }
-    response = requests.get(url, params=params)
-    if response.status_code != 200:
-        return {"error": "API 요청 실패", "statusCode": response.status_code, "details": response.text}
-    return response.json()
+@app.get("/")
+def root():
+    URL = "https://bigdata.kepco.co.kr/openapi/v1/powerUsage/industryType.do?year=2020&month=11&metroCd=11&cityCd=110&apiKey=xxx&returnType=json"
+    contents = requests.get(URL).text
+    return {"message": contents}
+
+@app.get("/home")
+def home():
+    return {"message": "Home!"}
